8393a1217875aa5f9a859f089aebb82173e05872
xoops123 <marine@mistgreen.net>
2013-12-15 11:54:52 +0900

    ほぼ使えるかな？
    機能追加、デザイン修正、不要ファイル削除など

--- html/themes/admin/language/english.php
+++ html/themes/admin/language/english.php
@@ -1,5 +1,49 @@
 <?php
-//global
+//for admin_theme
+define('_AD_THEME_SYSTEM_ADMIN', 'System Admin');
+
+define('_AD_THEME_BASIC_SETTING', 'Basic Settings');
+define('_AD_THEME_GENARAL_SETTING', 'General Settings');
+define('_AD_THEME_MAIL_SETTING', 'Mail Settings');
+define('_AD_THEME_PM_SETTING', 'PM Settings');
+define('_AD_THEME_META_SETTING', 'META Keywords ');
+define('_AD_THEME_BANNER_ADMIN', 'Banner');
+define('_AD_THEME_BW_SETTING', 'Banned Words');
+define('_AD_THEME_SEARCH_OPTION', 'Search Option');
+define('_AD_THEME_ACTION_SEARCH', 'Action Search');
+
+define('_AD_THEME_OP_ADMIN', 'Operating Admin');
+define('_AD_THEME_MODULES_ADMIN', 'Modules Admin');
+define('_AD_THEME_MODULES_INSTALL', 'Module Install');
+define('_AD_THEME_BLOCKS_ADMIN', 'Blocks Admin');
+define('_AD_THEME_BLOCKS_ADMIN2', 'Blocks  Admin(Altsys)');
+define('_AD_THEME_CB_ADMIN2', 'CustomBlockAdmin(altsys)');
+define('_AD_THEME_BLOCK_INSTALL', 'Block Install');
+define('_AD_THEME_CB_INSTALL', 'CustomBlock Install');
+define('_AD_THEME_THEMES_ADMIN', 'Themes Admin');
+define('_AD_THEME_COMMENT_ADMIN', 'Comment Adminn');
+define('_AD_THEME_IMAGE_MANAGER', 'Image Manager');
+define('_AD_THEME_SMILY_ICON', 'Smiky ICON');
+
+define('_AD_THEME_USER_ADMIN', 'User Admin');
+define('_AD_THEME_USER_SEARCH', 'User Search');
+define('_AD_THEME_USER_RANK', 'User Rank');
+define('_AD_THEME_GROUP_ADMIN', 'Group');
+define('_AD_THEME_AVATAR_ADMIN', 'Avatar');
+define('_AD_THEME_MAILJOB_ADMIN', 'Mail JOB');
+define('_AD_THEME_USER_SREG', 'User Shelf Reg');
+define('_AD_THEME_USER_DATA_DOWNLOAD', 'User Data Download');
+
+define('_AD_THEME_SITE_ADMIN', 'Site Settings');
+define('_AD_THEME_SITE_INFO', 'Site INFO');
+define('_AD_THEME_TEMPLATE_ADMIN', 'Template Admin');
+define('_AD_THEME_TEMPLATE_ADMIN2', 'Template Admin (Altsys)');
+define('_AD_THEME_TEMPLATE_ADMIN3', 'Template Admin EX (Altsys)');
+define('_AD_THEME_TEMPLATE_FILE_ADMIN', 'Template Files');
+define('_AD_THEME_LANGUAGE_ADMIN2', 'Languages Admin');
+define('_AD_THEME_HTACCESS_EXAMPLE', '.htaccess SAMPLE');
+define('_AD_THEME_SECURITY_GUIDE', 'Security Guide（protector）');
+
 define('_THEME_LANGUAGE_EVENT_INFO', 'Event Info');
 define('_THEME_LANGUAGE_WELCOME', 'Welcome');
 define('_THEME_LANGUAGE_WELCOME_2', '');
--- html/themes/admin/language/ja_utf8.php
+++ html/themes/admin/language/ja_utf8.php
@@ -1,5 +1,49 @@
 <?php
-//global
+//for admin_theme
+define('_AD_THEME_SYSTEM_ADMIN', 'システム管理');
+
+define('_AD_THEME_BASIC_SETTING', '基本設定');
+define('_AD_THEME_GENARAL_SETTING', '全般設定');
+define('_AD_THEME_MAIL_SETTING', 'メール設定');
+define('_AD_THEME_PM_SETTING', 'プライベートメッセージ基本設定');
+define('_AD_THEME_META_SETTING', 'メタ・キーワード・フッター設定等');
+define('_AD_THEME_BANNER_ADMIN', 'バナー管理');
+define('_AD_THEME_BW_SETTING', '禁止用語設定');
+define('_AD_THEME_SEARCH_OPTION', '検索オプション');
+define('_AD_THEME_ACTION_SEARCH', 'アクションサーチ');
+
+define('_AD_THEME_OP_ADMIN', '運用管理');
+define('_AD_THEME_MODULES_ADMIN', 'モジュールの管理');
+define('_AD_THEME_MODULES_INSTALL', 'モジュールのインストール');
+define('_AD_THEME_BLOCKS_ADMIN', 'ブロックの管理');
+define('_AD_THEME_BLOCKS_ADMIN2', 'ブロックの管理（altsys）');
+define('_AD_THEME_CB_ADMIN2', 'カスタムブロック管理（altsys）');
+define('_AD_THEME_BLOCK_INSTALL', 'ブロックのインストール');
+define('_AD_THEME_CB_INSTALL', 'カスタムブロックのインストール');
+define('_AD_THEME_THEMES_ADMIN', 'テーマの管理');
+define('_AD_THEME_COMMENT_ADMIN', 'コメント管理');
+define('_AD_THEME_IMAGE_MANAGER', 'イメージマネージャ');
+define('_AD_THEME_SMILY_ICON', 'スマイリーアイコンの管理');
+
+define('_AD_THEME_USER_ADMIN', 'ユーザー管理');
+define('_AD_THEME_USER_SEARCH', 'ユーザーの検索');
+define('_AD_THEME_USER_RANK', 'ユーザーランク管理');
+define('_AD_THEME_GROUP_ADMIN', 'グループ管理');
+define('_AD_THEME_AVATAR_ADMIN', 'アバターの管理');
+define('_AD_THEME_MAILJOB_ADMIN', 'メールジョブ一覧');
+define('_AD_THEME_USER_SREG', 'ユーザーの一括登録');
+define('_AD_THEME_USER_DATA_DOWNLOAD', 'ユーザーデータのダウンロード');
+
+define('_AD_THEME_SITE_ADMIN', 'サイト管理');
+define('_AD_THEME_SITE_INFO', 'サイト情報');
+define('_AD_THEME_TEMPLATE_ADMIN', 'テンプレート管理');
+define('_AD_THEME_TEMPLATE_ADMIN2', 'テンプレート管理（altsys）');
+define('_AD_THEME_TEMPLATE_ADMIN3', 'テンプレートの高度な操作（altsys）');
+define('_AD_THEME_TEMPLATE_FILE_ADMIN', 'テンプレートファイル管理');
+define('_AD_THEME_LANGUAGE_ADMIN2', '言語定数管理（altsys）');
+define('_AD_THEME_HTACCESS_EXAMPLE', '.htaccess 設定表示');
+define('_AD_THEME_SECURITY_GUIDE', 'セキュリティガイド（protector）');
+
 define('_THEME_LANGUAGE_EVENT_INFO', 'イベント通知設定');
 define('_THEME_LANGUAGE_WELCOME', 'ようこそ');
 define('_THEME_LANGUAGE_WELCOME_2', 'さん！');
@@ -8,7 +52,7 @@ define('_THEME_LANGUAGE_LOGIN', 'ログイン');
 define('_THEME_LANGUAGE_LOGIN_BUTTON', ' OK ');
 define('_THEME_LANGUAGE_LOGIN_FORM', ' ログインフォーム');
 define('_THEME_LANGUAGE_PWORD', 'パスワード：');
-define('_THEME_LANGUAGE_ADMIN_PAGE', '管理ページ');
+define('_THEME_LANGUAGE_AD_PAGE', '管理ページ');
 define('_THEME_LANGUAGE_LOGOUT', 'ログアウト');
 define('_THEME_LANGUAGE_LOST_PASS', 'パスワード紛失');
 define('_THEME_LANGUAGE_RESISTER', '新規登録しませんか？');
