9ba95f0ec3246571e5f19edf685ef520161d9b90
xoops123 <marine@mistgreen.net>
2013-12-15 12:00:23 +0900

    manifesto add

---
+++ html/themes/admin/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="admin"
+Depends=Legacy_RenderSystem,legacy
+Url="http://xoops123.com/"
+Version="0.06"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XCL html5 Style"
+Author=marine/mistgreen
+ScreenShot="screenshot.png"
+Description="Only Admin-Page Use"
+W3C=YES
+
+Licence="MIT"
+
+*/
+?>
