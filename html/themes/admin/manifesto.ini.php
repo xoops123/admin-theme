972d7903035499e1fa7acc1dffaa9deb7784acd3
xoops123 <marine@mistgreen.net>
2014-03-23 20:12:52 +0900

    テーマのreadme.txt読み込みを装備

--- html/themes/admin/manifesto.ini.php
+++ html/themes/admin/manifesto.ini.php
@@ -5,7 +5,7 @@
 Name="admin"
 Depends=Legacy_RenderSystem,legacy
 Url="http://xoops123.com/"
-Version="0.19"
+Version="0.20"
 
 [Theme]
 RenderSystem=Legacy_RenderSystem
--- html/themes/admin/readme.php
+++ html/themes/admin/readme.php
@@ -6,13 +6,21 @@
 <title>テーマの説明書</title>
 </head>
 <body>
-
 <?php
-$lines = file('readme.txt');
-foreach ($lines as $l) {
-  print $l . "<br>\n";
+
+mb_language('Japanese');
+ini_set('mbstring.internal_encoding', 'UTF-8');
+ini_set('mbstring.http_input'  , 'pass');
+ini_set('mbstring.http_output' , 'pass');
+ini_set('mbstring.substitute_character', 'none');
+
+$name = preg_replace('#[^0-9a-zA-Z_-]#', '', @$_GET['name']);
+$text = @file_get_contents('../'.$_GET['name'].'/readme.txt');
+if ($text) {
+	echo nl2br(htmlspecialchars(mb_convert_encoding($text, 'UTF-8', 'auto'), ENT_COMPAT, 'UTF-8'));
+} else {
+	echo $name.' テーマには説明書は用意されていません。';
 }
 ?>
-
 </body>
 </html>
